b1f64cb433a52d80fee9bc75e9876ca9a04e11aa
Viktor Rocha <viktoorrocha@gmail.com>
2018-10-20 16:02:04 -0400

    Inicio

---
+++ app/src/main/java/viktorrocha/br/com/carroskotlinapp/CarrosActivity.kt
@@ -0,0 +1,12 @@
+package viktorrocha.br.com.carroskotlinapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CarrosActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_carros)
+    }
+}
